5be9313822721b0edb4c9ec435be46583fb8234d
BlueSwan4 <violetcleak@hotmail.com>
2025-02-16 18:08:09 +0000

    Basic Movement
    Implemented a player sprite and created a move script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Variables
+    [SerializeField] private float movSpeed;
+    private float speedX;
+    private float speedY;
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        rb.velocity = new Vector2(speedX, speedY);
+    }
+}
