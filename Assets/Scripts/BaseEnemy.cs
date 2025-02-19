c8f5cf38fc896789cdf45dc0c936f6e951b063be
Nora Dwelly <dwelly.412@gmail.com>
2025-02-19 11:56:47 +0000

    Assigned basic stats to BasicEnemy

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -6,4 +6,11 @@ public class BaseEnemy : BaseUnit
 {
     //variables
     [SerializeField] private UnitDataSO enemyStats;
+
+    public void Start()
+    {
+        speed = 5;
+        attack = 5;
+        health = 10;
+    }
 }
