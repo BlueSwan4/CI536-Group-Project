33c69bd566988591e36c4cf6f372748c3a62e6a3
Nora Dwelly <dwelly.412@gmail.com>
2025-02-19 16:01:45 +0000

    Added basic random encounter checking - testing recquired

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,18 +13,34 @@ public class GameManager : MonoBehaviour
 
     public static event Action<GameState> OnGameStateChanged;
 
-    // Need to change Awake() so only creates new instance if one
-    // does not already exist. Also need to change to don't destoy on load
+    public GameObject playergameObj;
+
+    [Header("Random Encounter Data")]
+    public int stepsTakenInOverworld = 0;
+    public bool inSafeArea = false; // flag to limit encounters to "unsafe" areas
+
+
+    // TODO: change to don't destoy on load when we have extra game areas outside of the original and battle area
     void Awake()
     {
         if (Instance == null)
+        {
+            // set up game manager static instance
             Instance = this;
+            // load battle scene additively - we can then switch between this and the current overworld scene
+            SceneManager.LoadSceneAsync("BattleScene", LoadSceneMode.Additive);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void Start()
     {
         UpdateGameState(GameState.Wandering);
-        // note the time - we want to run a random encounter check at a regular interval
+        // note the steps taken - we want to run a random encounter check at a regular interval
+        stepsTakenInOverworld = 0;
     }
 
     public void UpdateGameState(GameState newState)
@@ -33,6 +50,7 @@ public class GameManager : MonoBehaviour
         switch(newState)
         {
             case GameState.Wandering:
+                stepsTakenInOverworld = 0;
                 break;
             case GameState.Fighting: 
                 // Activate the BattleManager
@@ -40,6 +58,44 @@ public class GameManager : MonoBehaviour
         }
 
         OnGameStateChanged?.Invoke(newState);
+    }
+
+    private void FixedUpdate()
+    {
+        switch (State)
+        {
+            case GameState.Fighting:
+                break;
+            case GameState.Wandering:
+                bool encounter = CheckForRandomEncounter();
+                if (encounter)
+                {
+                    // switch scene to battle scene
+                    // also move necessary objects to batle scene
+                    SceneManager.MoveGameObjectToScene(playergameObj, SceneManager.GetSceneByName("BattleScene"));
+                    SceneManager.MoveGameObjectToScene(transform.parent.gameObject, SceneManager.GetSceneByName("BattleScene"));
+
+                    // set battle scene as the active scene, then update game state
+                    SceneManager.SetActiveScene(SceneManager.GetSceneByName("BattleScene"));
+
+                    // update game state
+                    UpdateGameState(GameState.Fighting);
+                }
+                break;
+        }
+        
+    }
+
+    private bool CheckForRandomEncounter()
+    {
+        if (inSafeArea)
+            return false;
+
+        // use steps count to determine if we have encountered an enemy
+
+        float res = UnityEngine.Random.Range(0f, 50f) - stepsTakenInOverworld;
+
+        return res < 20; // TODO: replace this with the final random encounter formula
     }
 }
 
