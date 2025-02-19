ff97eccefcdf534c95a0c2979c02ec8d1f1a5d9e
Nora Dwelly <dwelly.412@gmail.com>
2025-02-19 11:55:15 +0000

    Assigned basic stats to player class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,9 @@ public class Player : BaseUnit
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 10;
+        attack = 5;
+        health = 20;
     }
 
     // Update is called once per frame
