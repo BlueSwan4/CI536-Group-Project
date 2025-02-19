c2dda3fba1bb0a5cb949b900b68dc2057cb5dd58
Nora Dwelly <dwelly.412@gmail.com>
2025-02-19 19:51:03 +0000

    Added script for storing references to scene roots

---
+++ Assets/Scripts/RootReferenceHolder.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootReferenceHolder : MonoBehaviour
+{
+    // used for storing references to root game objects when large amounts
+    // need to be either enabled or disabled
+    public GameObject rootObject;
+}
