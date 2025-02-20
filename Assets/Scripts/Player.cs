568424a2c3943bd64c15004b3a5def1c5718ba66
Nora Dwelly <dwelly.412@gmail.com>
2025-02-20 09:45:17 +0000

    Player class now listens for game stage change events

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,36 @@ using UnityEngine;
 public class Player : BaseUnit
 {
     // Start is called before the first frame update
+    public PlayerMovement movementScript;
+
     void Start()
     {
         speed = 10;
         attack = 5;
         health = 20;
+
+        // subscribe to battle events
+
+        GameManager.OnGameStateChanged += OnGameStateChanage;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnGameStateChanage(GameState newState)
+    {
+        switch (newState) 
+        {
+            case GameState.Wandering:
+                // enable movement
+                movementScript.enabled = true;
+                break;
+            case GameState.Fighting:
+                movementScript.enabled = false;
+                break;
+        }
     }
 }
