7ac5e40ca19f2e0dde9f0c3984e087ff694507b9
Nora Dwelly <dwelly.412@gmail.com>
2025-02-22 16:42:39 +0000

    Added turn end event and corresponding method to be subscribed to by the BattleManager class

--- Assets/Scripts/BaseUnit.cs
+++ Assets/Scripts/BaseUnit.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class BaseUnit : MonoBehaviour
 {
+    // unit events
+    public static event Action unitEndEvent;
 
     // variables (Shared between both player and enemy)
     public int health;
@@ -18,5 +22,11 @@ public class BaseUnit : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public static void EndUnitTurn()
+    {
+        // method responsible for ending the unit turn and raising the corresponding event
+        unitEndEvent.Invoke();
     }
 }
