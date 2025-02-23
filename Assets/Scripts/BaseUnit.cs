f35ed99afaec5afd334bebc24bf5a1ef4537a810
Nora Dwelly <dwelly.412@gmail.com>
2025-02-23 16:01:20 +0000

    Added methods to handle turn consumption and an event for unit death

--- Assets/Scripts/BaseUnit.cs
+++ Assets/Scripts/BaseUnit.cs
@@ -1,18 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.PackageManager;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class BaseUnit : MonoBehaviour
 {
     // unit events
     public static event Action unitEndEvent;
+    public static event Action<BaseUnit> unitDeathEvent; // raised when the unit dies
 
     // variables (Shared between both player and enemy)
     public int health;
     public int attack;
     public int speed;
+
+    public float attackDuration = 2.5f;
+
+    public BaseUnit target;
 
     void Start()
     {
@@ -22,6 +29,40 @@ public class BaseUnit : MonoBehaviour
     void Update()
     {
         
+    }
+    public virtual void UseTurn()
+    {
+
+    }
+
+    public virtual void ReceieveDamage(int amount)
+    {
+        // do damage reduction here
+        // overrides may add extra stuff
+        health -= amount;
+
+        // check if health <= 0
+        if (health <= 0)
+        {
+            health = 0;
+            // raise death event
+            unitDeathEvent.Invoke(this);
+        }
+    }
+
+
+    public IEnumerator UseBasicAttack()
+    {
+        // begins attack using a delay
+        // based off of Unity's Coroutine Documentation (2025). Acc: 23/02/2025
+        yield return new WaitForSeconds(attackDuration);
+        // deal damage to the target
+        if (target != null)
+        {
+            target.ReceieveDamage(attack);
+            // end the turn
+            EndUnitTurn();
+        }
     }
 
     public static void EndUnitTurn()
