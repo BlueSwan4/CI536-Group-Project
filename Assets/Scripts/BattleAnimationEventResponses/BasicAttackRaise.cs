8c9217300e16d199d6294d0d4f97bd97df928d8d
Nora Dwelly <dwelly.412@gmail.com>
2025-02-24 11:13:41 +0000

    [Deprecated]

--- Assets/Scripts/BattleAnimationEventResponses/BasicAttackRaise.cs
+++ Assets/Scripts/BattleAnimationEventResponses/BasicAttackRaise.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class BasicAttackRaise : MonoBehaviour
 {
+    // make sure we have an animation controller reference
+
     // Start is called before the first frame update
     public void RaiseAttackEventEnd()
     {
         // this is called when the basic attack animation is ended to raise the turn end event
         BaseUnit.EndUnitTurn();
+    }
+
+    public void StartAttackAnim()
+    {
+
+    }
+
+    public void StartDamageAnim()
+    {
+
     }
 }
